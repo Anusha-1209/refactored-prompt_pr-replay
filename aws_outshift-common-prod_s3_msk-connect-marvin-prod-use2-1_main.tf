2eb1c8b1672dc48fa688c95586e0e0205ea3ddbf
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-30 18:30:29 -0400

    -

--- aws_outshift-common-prod_s3_msk-connect-marvin-prod-use2-1_main.tf
+++ aws_outshift-common-prod_s3_msk-connect-marvin-prod-use2-1_main.tf
@@ -17,7 +17,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-prod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/outshift-common-prod/us-east-2/s3/msk-sink-connect-marvin-prod-use2-1.tfstate"
+    key = "terraform-state/aws/outshift-common-prod/us-east-2/s3/msk-connect-sink-marvin-prod-use2-1.tfstate"
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
