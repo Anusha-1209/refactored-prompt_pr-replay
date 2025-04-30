8a409071b1783296ebeb11a9993d2986a9f31aa3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-30 19:10:05 -0400

    Creating Base Branch for PR 801

---
+++ aws_outshift-common-dev_us-east-2_eks_gitops-dev-use2-1_provider.tf
@@ -0,0 +1,29 @@
+# This file was created by Outshift Platform Self-Service automation.
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.eks_aws_account}/terraform_admin"
+}
+
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "OPEng Platform"
+      Component          = "baseapps"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Restricted"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "Outshift SRE"
+    }
+  }
+}
