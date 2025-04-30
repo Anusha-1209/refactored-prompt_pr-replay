1876de2c74d97cfb9c268933d15dbc7ea33fe0d7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-30 18:26:36 -0400

    -

---
+++ aws_outshift-common-prod_s3_marvin-prod-use2-1-msk-s3-connectors_main.tf
@@ -0,0 +1,51 @@
+# This provider allows access to the eticloud/eticcprod namespace in Keeper. Do not modify it without discussing with the SRE team.
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+# Change `path = "secret/eticcprod/infra/<account_name>/aws" to specify the account in which the resources will be created.
+# Must match the account in which the VPC was created.
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/apisec-prod/terraform_admin"
+}
+
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-prod" # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/outshift-common-prod/us-east-2/s3/marvin-prod-use2-1-msk-s3-connectors.tfstate"
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+
+  }
+}
+
+variable "AWS_INFRA_REGION" {
+  description = "AWS Region"
+  default     = "us-east-2" #Set the region for the resources to be created.
+}
+# Infra AWS Provider
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = var.AWS_INFRA_REGION
+  max_retries = 3
+}
+
+module "s3" {
+  source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
+  bucket_name = "marvin-prod-use2-1-msk-s3-connectors" # The name of the S3 bucket.  S3 bucket names are required to be globally unique across all of AWS.
+  #Continuous Security Buddy Tags.
+  #For more information, see the CSB tagging Sharepoint page here:
+  #https://cisco.sharepoint.com/Sites/CSB/SitePages/Security%20Tagging%20and%20Audit%20in%20AWS.aspx
+  CSBDataClassification = "Cisco Restricted"
+  CSBEnvironment        = "Prod"
+  CSBApplicationName    = "Marvin"
+  CSBResourceOwner      = "Outshift SRE"
+  CSBCiscoMailAlias     = "eti-sre@cisco.com"
+  CSBDataTaxonomy       = "Cisco Operations Data"
+}
