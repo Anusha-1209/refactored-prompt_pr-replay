a37e13d24ea339054701010d4b21bf5f31f60a6c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-30 20:05:02 -0400

    chore: apply IAMReadOnlyAccess permissions to ventures IAM roles

--- modules_sso-roles_main.tf
+++ modules_sso-roles_main.tf
@@ -11,3 +11,9 @@ resource "aws_iam_role_policy_attachment" "power_user_access_policy_attachment" 
   role       = aws_iam_role.this.name
   policy_arn = var.policy_arn
 }
+
+# Attach the IAMReadOnlyAccess AWS Managed policy
+resource "aws_iam_role_policy_attachment" "iam_read_only_policy_attachment" {
+  role       = aws_iam_role.this.name
+  policy_arn = var.iam_read_only_policy_arn
+}
