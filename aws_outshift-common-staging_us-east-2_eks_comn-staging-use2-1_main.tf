2bfdffc7d52bcca6d0dff56df8478fe21323326f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-30 20:08:42 -0400

    Creating Base Branch for PR 838

---
+++ aws_outshift-common-staging_us-east-2_eks_comn-staging-use2-1_main.tf
@@ -0,0 +1,38 @@
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-nonprod"
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/outshift-common-staging/us-east-2/eks/comn-staging-use2-1.tfstate"
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+  }
+}
+
+locals {
+  name             = "comn-staging-use2-1"
+  region           = "us-east-2"
+  aws_account_name = "outshift-common-staging"
+}
+
+module "eks_all_in_one" {
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+
+
+  name               = local.name             # EKS cluster name
+  region             = local.region           # AWS provider region
+  aws_account_name   = local.aws_account_name # AWS account name
+  cidr               = "10.0.0.0/16"          # VPC CIDR
+  public_subnet_mask = 196
+  intra_subnet_mask  = 200
+  cluster_version    = "1.30"                 # EKS cluster version
+
+  # EKS Managed Private Node Group
+  instance_types = ["m6a.2xlarge"] # EKS instance types
+  min_size       = 1               # EKS node group min size
+  max_size       = 1               # EKS node group max size
+  desired_size   = 1               # EKS node group desired size
+
+  # Karpenter
+  create_karpenter_irsa = true #  Create Karpenter IRSA
+}
