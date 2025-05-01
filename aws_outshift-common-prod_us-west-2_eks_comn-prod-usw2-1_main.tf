12bcacf9f03e8483e59edcf56da5bea054f577df
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-05-01 13:46:23 -0400

    Creating Base Branch for PR 852

---
+++ aws_outshift-common-prod_us-west-2_eks_comn-prod-usw2-1_main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-prod"
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/outshift-common-prod/us-west-2/eks/comn-prod-usw2-1.tfstate"
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+  }
+}
+
+locals {
+  name             = "comn-prod-usw2-1"
+  region           = "us-west-2"
+  aws_account_name = "outshift-common-prod"
+}
+
+module "eks_all_in_one" {
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+
+  name             = local.name             # EKS cluster name
+  region           = local.region           # AWS provider region
+  aws_account_name = local.aws_account_name # AWS account name
+  cidr             = "10.2.0.0/16"          # VPC CIDR
+  cluster_version  = "1.30"                 # EKS cluster version
+
+  # EKS Managed Private Node Group
+  instance_types = ["m6a.2xlarge"] # EKS instance types
+  min_size       = 1               # EKS node group min size
+  max_size       = 2               # EKS node group max size
+  desired_size   = 1               # EKS node group desired size
+
+  # Karpenter
+  create_karpenter_irsa = true # Create Karpenter IRSA
+}
