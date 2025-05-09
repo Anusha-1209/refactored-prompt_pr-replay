bd7f45ff47f110f00de8dd3df5aaea549920b6c3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-05-09 18:02:21 -0400

    chore: deploy cloudfront for phoenix-ui

---
+++ aws_outshift-common-dev_us-east-2_cdn_phoenix-ui_locals.tf
@@ -0,0 +1,6 @@
+locals {
+  aws_account         = "dragonfly-dev"
+  cdn_domain_name     = "cdr-ui.dev.panoptica.app"
+  route53_account     = "eticloud"
+  bucket_domain_name  = "dragonfly-dev-cdr-ui.s3.eu-west-1.amazonaws.com"
+}
