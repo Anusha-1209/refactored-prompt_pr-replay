6237ba3e6e01d482ba23026e54db2b647aa70efe
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-05-09 17:11:43 -0400

    chore: updates

---
+++ aws_outshift-common-dev_cdn_phoenix.dev.outshift.ai_dev_locals.tf
@@ -0,0 +1,6 @@
+locals {
+  aws_account         = "outshift-common-dev"
+  cdn_domain_name     = "phoenix-ui.dev.app"
+  route53_account     = "eticloud"
+  bucket_domain_name  = "outshift-phoenix-ui.s3.us-east-2.amazonaws.com"
+}
