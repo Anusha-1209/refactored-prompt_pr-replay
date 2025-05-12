f48785c25995dc2603f21594e889534cf3eab2bc
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-05-12 19:44:08 -0400

    chore: updates

--- aws_outshift-common-dev_us-east-2_cdn_phoenix-ui_provider.tf
+++ aws_outshift-common-dev_us-east-2_cdn_phoenix-ui_provider.tf
@@ -43,6 +43,26 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias       = "us-east-1"
+  region      = "us-east-1"
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  max_retries = 3
+
+  default_tags {
+    tags = {
+      ApplicationName    = "outshift_ventures"
+      Component          = "phoenix"
+      CiscoMailAlias     = "outshift-phoenix@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "outshift-phoenix-admins"
+    }
+  }
+}
+
+provider "aws" {
   alias       = "route53"
   region      = "us-east-1"
   access_key  = data.vault_generic_secret.aws_infra_credential_route53.data["AWS_ACCESS_KEY_ID"]
